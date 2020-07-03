be7062a49730e814d677b86c8e0aa2b93eaeb76b
yuemenglong <yuemenglong@126.com>
2020-07-03 14:44:16 +0800

    add: 开始解决解密的问题

---
+++ jvmti/decrypt/run.sh
@@ -0,0 +1,2 @@
+java -jar -agentlib:decrypt xxx.jar
+java -jar -agentpath:"full_path" xxx.jar
---
+++ src/main/java/encrypt/Test.java
@@ -0,0 +1,7 @@
+package encrypt;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This Is A encrypt.Test Class");
+    }
+}
